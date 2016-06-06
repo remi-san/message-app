2a94c4ea046302598fd0fed10f8acacb038522e5
remi-san <remi.san@gmail.com>
2016-06-06 20:12:25 +0000

    moved classes from mini-game-message-app

---
+++ src/Message/MessageTextExtractor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MessageApp\Message;
+
+interface MessageTextExtractor
+{
+    /**
+     * Extract the message from the game result.
+     *
+     * @param  object $object
+     * @param  string $languageIso
+     * @return string
+     */
+    public function extractMessage($object, $languageIso);
+}
